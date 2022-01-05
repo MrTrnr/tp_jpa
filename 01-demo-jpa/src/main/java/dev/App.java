a5ff371e1e105ec4333a9f090746c89b4e4e34c4
Ross <rossi.oddet@dev-institut.fr>
2022-01-05 11:58:52 +0100

    premier projet JPA

---
+++ 01-demo-jpa/src/main/java/dev/App.java
@@ -0,0 +1,55 @@
+package dev;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
+import javax.persistence.TypedQuery;
+
+import dev.entites.Pizza;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = null;
+
+		try {
+			emf = Persistence.createEntityManagerFactory("pizza-pu");
+
+			EntityManager em = emf.createEntityManager();
+			
+			
+			Pizza pizzaId1 = em.find(Pizza.class, 1);
+			System.out.println("Pizza id=1 name=" + pizzaId1.getName());
+			
+			TypedQuery<Pizza> query = em.createQuery("select p from Pizza p", Pizza.class);
+			
+			List<Pizza> listePizzas = query.getResultList();
+			
+			for (Pizza pizza : listePizzas) {
+				System.out.println(pizza.getName());
+			}
+			
+
+			em.close();
+			
+			System.out.println("Pizza id=1 name=" + pizzaId1.getName());
+		} 
+		catch (PersistenceException e) {
+			System.err.println("Erreur de persistance : " + e.getMessage());
+		}
+		
+		finally {
+			if (emf != null) {
+				emf.close();
+			}
+		}
+
+		
+
+	}
+
+}
